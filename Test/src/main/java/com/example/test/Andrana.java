10ae55db78eda241afead816ef8aaab6d3e016cf
vals25-mg <119032319+vals25-mg@users.noreply.github.com>
2023-03-31 08:52:24 +0300

    sprint4

---
+++ Test/src/main/java/com/example/test/Andrana.java
@@ -0,0 +1,16 @@
+package comtest;
+
+import etu2034.framework.annotation.MethodAnnotation;
+//import et
+
+public class Andrana {
+
+    public static void main(String[] args) {
+
+    }
+
+    @MethodAnnotation(url="huhu")
+    public void huhu(){
+        System.out.println("hello world");
+    }
+}
